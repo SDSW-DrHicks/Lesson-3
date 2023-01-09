01524f571d34a7ce757045b8044d127f51abefe6
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2023-01-09 08:55:37 -0500

    First draft

--- NestedLoops.py
+++ NestedLoops.py
@@ -2,3 +2,22 @@
 # Nested Loops
 # ----------------------------------------------
 
+# Here is the general structure of a nested set of loops:
+
+# outer for loop
+#for element in sequence
+#   # inner for loop
+#   for element in sequence:
+#       body of inner for loop
+#   body of outer for loop
+
+# Example: Generate a multiplication table
+
+# outer loop
+for i in range(1, 11):
+    # nested loop
+    # to iterate from 1 to 10
+    for j in range(1, 11):
+        # print multiplication
+        print(i * j, end=' ')
+    print()
